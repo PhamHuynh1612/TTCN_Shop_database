7990e64a1ddcf31ca4bb8c8eaaa8c5bb975c26ea
huynh <huynhpham493@gmail.com>
2023-05-31 00:11:08 +0700

    add backup file

--- ban_laptop_localhost__3_-2023_05_31_00_06_32-dump.sql
+++ ban_laptop_localhost__3_-2023_05_31_00_06_32-dump.sql
@@ -34,11 +34,9 @@ CREATE TABLE `admin` (
 -- Dumping data for table `admin`
 --
 
-LOCK TABLES `admin` WRITE;
 /*!40000 ALTER TABLE `admin` DISABLE KEYS */;
 INSERT INTO `admin` VALUES ('admin','admin',1),('admin','admin',2),('admin','admin',3);
 /*!40000 ALTER TABLE `admin` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `cartitem`
@@ -72,10 +70,8 @@ CREATE TABLE `cartitem` (
 -- Dumping data for table `cartitem`
 --
 
-LOCK TABLES `cartitem` WRITE;
 /*!40000 ALTER TABLE `cartitem` DISABLE KEYS */;
 /*!40000 ALTER TABLE `cartitem` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `color`
@@ -96,11 +92,9 @@ CREATE TABLE `color` (
 -- Dumping data for table `color`
 --
 
-LOCK TABLES `color` WRITE;
 /*!40000 ALTER TABLE `color` DISABLE KEYS */;
 INSERT INTO `color` VALUES (1,'đen',1),(2,'trắng',1),(3,'xám',1),(4,'bạc',1);
 /*!40000 ALTER TABLE `color` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `comment`
@@ -126,10 +120,8 @@ CREATE TABLE `comment` (
 -- Dumping data for table `comment`
 --
 
-LOCK TABLES `comment` WRITE;
 /*!40000 ALTER TABLE `comment` DISABLE KEYS */;
 /*!40000 ALTER TABLE `comment` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `orderdetail`
@@ -150,10 +142,8 @@ CREATE TABLE `orderdetail` (
 -- Dumping data for table `orderdetail`
 --
 
-LOCK TABLES `orderdetail` WRITE;
 /*!40000 ALTER TABLE `orderdetail` DISABLE KEYS */;
 /*!40000 ALTER TABLE `orderdetail` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `orderitem`
@@ -186,10 +176,8 @@ CREATE TABLE `orderitem` (
 -- Dumping data for table `orderitem`
 --
 
-LOCK TABLES `orderitem` WRITE;
 /*!40000 ALTER TABLE `orderitem` DISABLE KEYS */;
 /*!40000 ALTER TABLE `orderitem` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `paymentdetail`
@@ -220,10 +208,8 @@ CREATE TABLE `paymentdetail` (
 -- Dumping data for table `paymentdetail`
 --
 
-LOCK TABLES `paymentdetail` WRITE;
 /*!40000 ALTER TABLE `paymentdetail` DISABLE KEYS */;
 /*!40000 ALTER TABLE `paymentdetail` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `product`
@@ -260,11 +246,9 @@ CREATE TABLE `product` (
 -- Dumping data for table `product`
 --
 
-LOCK TABLES `product` WRITE;
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
 INSERT INTO `product` VALUES (1,3,'\"MacBook Air 13 2020 M1 256GB\"','\"Chiếc MacBook Air có hiệu năng đột phá nhất từ trước đến nay đã xuất hiện. Bộ vi xử lý Apple M1 hoàn toàn mới đưa sức mạnh của MacBook Air M1 13 inch 2020 vượt xa khỏi mong đợi người dùng, có thể chạy được những tác vụ nặng và thời lượng pin đáng kinh ngạc.\"',1,'\"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/macbook-air-gold-select-201810?wid=539&hei=312&fmt=jpeg&qlt=95&.v=1664472289059\"',10,18490000,3,1,'recommendProduct'),(2,3,'\"MacBook Pro M2 2022 13 inch 8CPU 10GPU 8GB 256GB\"','\"Với sự giúp sức của chip M2, MacBook Pro 2022 trở nên chuyên nghiệp hơn bao giờ hết. Kết cấu nhỏ nhắn, thời lượng pin 20 giờ và hệ thống làm mát hiệu quả giúp sản phẩm duy trì hiệu suất cao trong thời gian dài. Màn hình Retina, camera FaceTime HD và micro studio cũng là điểm cộng đem đến trải nghiệm xuất sắc.\"',1,'\"https://cdn.tgdd.vn/Products/Images/44/282828/apple-macbook-pro-13-inch-m2-2022-1-1.jpg\"',10,29890000,3,1,'recommendProduct'),(3,3,'\"MacBook Pro M2 2022 13 inch 8CPU 10GPU 8GB 256GB\"','\"Với sự giúp sức của chip M2, MacBook Pro 2022 trở nên chuyên nghiệp hơn bao giờ hết. Kết cấu nhỏ nhắn, thời lượng pin 20 giờ và hệ thống làm mát hiệu quả giúp sản phẩm duy trì hiệu suất cao trong thời gian dài. Màn hình Retina, camera FaceTime HD và micro studio cũng là điểm cộng đem đến trải nghiệm xuất sắc.\"',1,'\"https://cdn.tgdd.vn/Products/Images/44/282828/apple-macbook-pro-13-inch-m2-2022-1-1.jpg\"',10,29890000,4,1,'recommendProduct'),(4,2,'\"Laptop Asus Vivobook M513UA-EJ704W R7 5700U\"','\"Asus Vivobook M513UA-EJ704W mang trong mình phong cách thiết kế đơn giản, kèm theo màn hình lớn 15.6 inch. Máy được trang bị CPU AMD Ryzen 7 kèm theo 8GB RAM và SSD 512GB, đủ sức để đáp ứng nhiều loại nhu cầu khác nhau từ làm việc đến giải trí.\"',1,'\"https://cdn2.cellphones.com.vn/x358,webp,q100/media/catalog/product/t/e/text_d_i_19.png\"',10,13990000,4,3,'saleProduct'),(5,2,'\"Laptop Asus Vivobook X415EA-EK1387W i3 1115G4\"','\"Asus Vivobook X415EA-EK1387W dành cho những ai đang tìm kiếm một chiếc laptop nhỏ gọn, thiết kế đẹp và hiệu năng đủ tốt để chạy các ứng dụng văn phòng mượt mà. Với mức giá khá rẻ, không khó để VivoBook X415 chinh phục người dùng.\"',1,'\"https://cdn2.cellphones.com.vn/x358,webp,q100/media/catalog/product/8/_/8_10_16.jpg\"',10,9890000,4,2,'saleProduct'),(6,2,'\"Laptop Dell Inspiron 3511 5829BLK\"','\"Laptop Dell Inspiron 3511 5829BLK - Hiệu năng mạnh mẽ cân mọi tác vụ học tập, văn phòng\"',1,'\"https://cdn2.cellphones.com.vn/x358,webp,q100/media/catalog/product/t/e/text_ng_n_3__1_71.png\"',10,13490000,1,3,'saleProduct'),(7,4,'\"Laptop Lenovo Ideapad Gaming 3 15ARH7\"','\"Laptop Lenovo Ideapad Gaming 3 15ARH7 - Laptop cho trải nghiệm chiến game ấn tượng\"',1,'\"https://cdn2.cellphones.com.vn/x358,webp,q100/media/catalog/product/1/h/1h47.png\"',50,23990000,2,3,'saleProduct');
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `productcategory`
@@ -285,11 +269,9 @@ CREATE TABLE `productcategory` (
 -- Dumping data for table `productcategory`
 --
 
-LOCK TABLES `productcategory` WRITE;
 /*!40000 ALTER TABLE `productcategory` DISABLE KEYS */;
 INSERT INTO `productcategory` VALUES (1,'\"example\"',0),(2,'\"Laptop văn phòng\"',1),(3,'\"MacBook\"',1),(4,'\"Laptop Gaming\"',1);
 /*!40000 ALTER TABLE `productcategory` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `shoppingsession`
@@ -311,10 +293,8 @@ CREATE TABLE `shoppingsession` (
 -- Dumping data for table `shoppingsession`
 --
 
-LOCK TABLES `shoppingsession` WRITE;
 /*!40000 ALTER TABLE `shoppingsession` DISABLE KEYS */;
 /*!40000 ALTER TABLE `shoppingsession` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `size`
@@ -335,11 +315,9 @@ CREATE TABLE `size` (
 -- Dumping data for table `size`
 --
 
-LOCK TABLES `size` WRITE;
 /*!40000 ALTER TABLE `size` DISABLE KEYS */;
 INSERT INTO `size` VALUES (1,'13.3',1),(2,'14',1),(3,'15.6',1),(4,'16',1);
 /*!40000 ALTER TABLE `size` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
@@ -364,11 +342,9 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
 INSERT INTO `user` VALUES (254,'\"123865@gmail.com\"','huynh','\"huynh123\"','null','ha noi',1),(302,'\"13865@gmail.com\"','null','\"huynh123\"','null','null',1),(352,'\"1365@gmail.com\"','null','\"huynh123\"','null','null',1),(402,'\"huynhpham493@gmail.com\"','null','\"123asd\"','null','null',1),(452,'\"hdgfts@gmail.com\"','abc','\"123acb\"','null','null',1);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `user_seq`
@@ -386,11 +362,9 @@ CREATE TABLE `user_seq` (
 -- Dumping data for table `user_seq`
 --
 
-LOCK TABLES `user_seq` WRITE;
 /*!40000 ALTER TABLE `user_seq` DISABLE KEYS */;
 INSERT INTO `user_seq` VALUES (551);
 /*!40000 ALTER TABLE `user_seq` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -401,4 +375,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-05-30 23:56:00
+-- Dump completed on 2023-05-31  0:06:32
